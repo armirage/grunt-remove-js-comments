4bb8347d7e1a94ea3fc4415253fc9f64a92ad5a6
Ray (Clinton) Mulligan <12667615+ClintMulligan@users.noreply.github.com>
2020-10-01 07:42:06 -0400

    Update index.js
    Add variety of comments and hashbang  comment.

--- tests/index.js
+++ tests/index.js
@@ -1,13 +1,27 @@
+#!/usr/bin/env node
 /*# Title: @armirage/grunt-remove-js-comments #*/
 /*! Attribution comments, I want to keep these !*/
+
+// ANCHOR Start of code here
 
 /**
  * @Module JSDocs
  * @function hello
  */
 
+/*
+ Multi line comments can go
+ except I want to keep the JSDocs for API documentation
+ */
+
 function hello() {
+	// returns a string
 	return "Hello";
 }
-//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+// This can go but keep the line to separate the page...
+// Management loves these annoying decorations for some reason.
+//-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
+
+/* Logs results */
 console.log( hello(), "World" );
