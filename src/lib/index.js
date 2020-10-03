87a9d96e5075408f052c3c8921666f3d5ac88e6c
Ray (Clinton) Mulligan <12667615+ClintMulligan@users.noreply.github.com>
2020-10-03 00:11:47 -0400

    change syntax of comments

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,10 @@
-/*! version: 1.0.0 !*/
-/*! author: Clinton Mulligan !*/
-/*! license: BSD 3-Clause License !*/
-/*! Copyright (c) 2020, Clinton Mulligan. All rights reserved. !*/
-/*! https://github.com/armirage/eslint-config-armirage/blob/master/LICENSE.md !*/
+/*!
+  version: 1.0.0
+  author: Clinton Mulligan
+  license: BSD 3-Clause License
+  Copyright (c) 2020, Clinton Mulligan. All rights reserved.
+  https://github.com/armirage/eslint-config-armirage/blob/master/LICENSE.md
+!*/
 
 module.exports = function (content, options) {
   const removeBlockAndLine = require('./removeComments');
